f3ff64ab42c8e545517245490b64e196ea4d1ce5
Mateusz MatejczykIIUJ-MateuszMatejczyk <mateusz.matejczyk@gmail.com>
2013-05-12 22:39:17 +0200

    Merged i003_z001 with i003

--- Bomberman/Bomberman/Engine.cs
+++ Bomberman/Bomberman/Engine.cs
@@ -92,22 +92,6 @@ namespace Bomberman
             AddObject(player = p);
         }
 
-        private int level = 1;
-        private int score = 0;
-
-        [DataMember()]
-        public int Level
-        {
-            get { return level; }
-            set{ level = value;}
-        }
-        [DataMember()]
-        public int Score
-        {
-            get{ return score;}
-            set{ score = value;}
-        }
-
         private List<GameObject> gameObjects = new List<GameObject>();
 
         /// <summary>
