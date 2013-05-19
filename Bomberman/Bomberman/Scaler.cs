725f5eb86baeffdc1d2e13a6937bf209dae28eb4
Mateusz MatejczykIIUJ-MateuszMatejczyk <mateusz.matejczyk@gmail.com>
2013-05-19 20:25:34 +0200

    Fluent movement for player

--- Bomberman/Bomberman/Scaler.cs
+++ Bomberman/Bomberman/Scaler.cs
@@ -41,5 +41,23 @@ namespace Bomberman
         public int blockHeight( ){
             return Maze.ScreenHeight / (Maze.Height + OFFSET);
         }
+
+        public Vector2 Transform(int x, int y)
+        {
+            return new Vector2(x * Maze.BlockWidth, (y + OFFSET) * Maze.BlockHeight);//
+        }
+        public Vector2 Transform(uint x, uint y)
+        {
+            return Transform((int)x, (int)y);
+        }
+        public Vector2 Transform(Point p)
+        {
+            return Transform(p.X, p.Y);
+        }
+
+        public Rectangle GetRectangle(Vector2 v)
+        {
+            return new Rectangle((int)v.X, (int)v.Y, Maze.BlockWidth, Maze.BlockHeight);
+        }
     }
 }
